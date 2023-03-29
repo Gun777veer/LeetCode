88cce2280a74a4f1c9750aa4b5fd046590c94fb0
Gunveer Singh <72687410+Gun777veer@users.noreply.github.com>
2023-03-29 23:15:32 +0530

    Create stringmethods.java

---
+++ strings/stringmethods.java
@@ -0,0 +1,46 @@
+
+public class stringmethods {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//methods
+		
+//		String s="gunveer Singh";
+//		
+//		String a=s.toUpperCase();
+//		String a1=s.toLowerCase();
+//
+//		 
+//		System.out.println(a+" "+a1+" "+a.substring(8,13));
+//		
+//		
+//		if(a.compareTo(a1)==0) {
+//			System.out.println("Strings are equal");
+//		}
+//		else if(a.compareTo(a1.toUpperCase())==0) {
+//			System.out.println("Strings are equal but in different cases");
+//		}
+//		
+
+		
+		//Strings are immutable
+		
+		/*
+		 * when you change the value of an already existing string variable
+		 * java doesn't its value in the memory. We can't change the string objects but we can
+		 * change the position so that it points to a new object that we've created in the memory.
+		 * 
+		 * It helps java save memory
+		 * 
+		 * when we create an string the object of that is stored in an string pool so if the same 
+		 * object is created again it will check the pool and if its there it point to it.
+		 * 
+		 * if you want to create a separate object use the new keyword
+		 * 
+		 * 
+		 * */
+		
+	}
+
+}
