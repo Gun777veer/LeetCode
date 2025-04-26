1582bdf008798a35ee0a9ece946412bf80b26755
Gunveer Singh <72687410+Gun777veer@users.noreply.github.com>
2025-04-26 23:31:50 +0530

    Create count_subarrays.cpp

---
+++ Daily_Ques/count_subarrays.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    long long countSubarrays(vector<int>& nums, int minK, int maxK) {
+        int n = nums.size();
+        long long count = 0;
+        int lastMin = -1, lastMax = -1, lastInvalid = -1;
+
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] < minK || nums[i] > maxK) {
+                lastInvalid = i;
+            }
+            if (nums[i] == minK) {
+                lastMin = i;
+            }
+            if (nums[i] == maxK) {
+                lastMax = i;
+            }
+
+            int minPos = min(lastMin, lastMax);
+            if (minPos > lastInvalid) {
+                count += (minPos - lastInvalid);
+            }
+        }
+
+        return count;
+    }
+};
