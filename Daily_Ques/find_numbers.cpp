16c7396db1bf62847b6a6f3700427d06fbcef2d7
Gunveer Singh <72687410+Gun777veer@users.noreply.github.com>
2025-04-30 23:20:31 +0530

    Create find_numbers.cpp

---
+++ Daily_Ques/find_numbers.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+#include <cmath>
+using namespace std;
+
+class Solution {
+public:
+    int findNumbers(vector<int>& nums) {
+        int count = 0;
+        for (int num : nums) {
+            int digits = log10(num) + 1;
+            if (digits % 2 == 0)
+                count++;
+        }
+        return count;
+    }
+};
